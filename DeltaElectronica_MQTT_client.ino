795bd96c177360d1d0d178e6d3270316e8a1fabe
Alfons Schuck <schuck@schuck-macpro.huth.physik.ffm.de>
2020-07-24 16:54:45 +0200

    DAC Control works via MQTT

--- DeltaElectronica_MQTT_client.ino
+++ DeltaElectronica_MQTT_client.ino
@@ -1,28 +1,32 @@
-#include <Wire.h>
 #include <MCP4725.h>
+#include <Wire.h>
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h> // MQTT Publish nur mit QoS 0 möglich (Speicher ist zu knapp)
-
 
 // Delta Electronica properties
 unsigned int voltage_max = 30;
 unsigned int current_max = 5;
 
 // Variables for Analog Inputs connected to Delta Electronica monitor output
-unsigned int voltage_sensor;
-unsigned int current_sensor;
-short voltage_measured;
-short current_measured;
+short voltage_sensor;
+short current_sensor;
+
+//Variables for Analog Outputs
+short voltage_output;
+short current_output;
+
+char voltage_sensor_str[8];
+char current_sensor_str[8];
 
 // Address of MCP4725 board
-MCP4725 DAC(0x60); //0x60 default address
+MCP4725 dac(0x60); //0x60 default address
 
 // MQTT Network Parameters
 // Set your MAC address and IP address here
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(10, 42, 0, 10); // DHCP possible, but expensive
-IPAddress server_ip(192,168,2,161);
+IPAddress server_ip(192, 168, 2, 161);
 
 // Set MQTT Topic to publish and subscribe
 char topic_current_publish[] = "mbe/load-lock-lamp/current/update";
@@ -34,58 +38,60 @@ char topic_voltage_subscribe[] = "mbe/load-lock-lamp/voltage/targetstate";
 EthernetClient ethClient;
 PubSubClient mqttClient(ethClient);
 
-
 void setup()
 {
   // Useful for debugging purposes
   Serial.begin(9600);
-  
+
   // Start the ethernet connection
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP"); 
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("Failed to configure Ethernet using DHCP");
   }
-  else {
-  Serial.println(Ethernet.localIP());
+  else
+  {
+    Serial.println(Ethernet.localIP());
   }
   // Ethernet takes some time to boot!
-  delay(3000);                          
- 
-  
+  delay(3000);
+
   // Set the MQTT server to the server stated above ^
-  mqttClient.setServer(server_ip, 1883);   
- 
+  mqttClient.setServer(server_ip, 1883);
+
   // Attempt to connect to the server with the ID "myClientID"
-  if (mqttClient.connect("LoadLock")) 
+  if (mqttClient.connect("LoadLock"))
   {
     Serial.println("Connection has been established, well done");
- 
+
     // Establish the subscribe event
     mqttClient.setCallback(subscribeReceive);
-  } 
-  else 
+  }
+  else
   {
     Serial.println("Looks like the server connection failed...");
   }
 
-  //DAC.begin();
-  //DAC.setValue(0);
+  dac.begin();
+  dac.setValue(0);
+  
+   // If control is needed
+  mqttClient.subscribe(topic_current_subscribe, 1);
+  mqttClient.subscribe(topic_voltage_subscribe, 1);
 
-  // Set A0 and A1 as Inputs
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
 }
 
 void loop()
 {
   // This is needed at the top of the loop!
   mqttClient.loop();
- 
-  // If control is needed
-  // mqttClient.subscribe(topic_prefix + topic_subscribe + topic_suffix_voltage);
- 
-  // Attempt to publish a value to the topic "MakerIOTopic"
 
-  if(mqttClient.publish(topic_current_publish, "Hello World"))
+  // Attempt to publish a value to the topic "topic_*_publish"
+  voltage_sensor = analogRead(A0) * (voltage_max / 1023.0);
+  dtostrf(voltage_sensor, 6,2, voltage_sensor_str);
+  current_sensor = analogRead(A1) * (current_max / 1023.0);
+   dtostrf(current_sensor, 6,2, current_sensor_str);
+
+  if (mqttClient.publish(topic_current_publish, current_sensor_str))
   {
     Serial.println("Publish message success");
   }
@@ -93,24 +99,37 @@ void loop()
   {
     Serial.println("Could not send message :(");
   }
- 
+  mqttClient.publish(topic_voltage_publish, voltage_sensor_str);
+
   // Dont overload the server!
   delay(4000);
 }
 
-void subscribeReceive(char* topic, byte* payload, unsigned int length)
+void subscribeReceive(char *topic, byte *payload, unsigned int length)
 {
+  String str_message = "";
+  for (int i=0;i<length;i++) {
+    str_message += ((char)payload[i]);
+  }
   // Print the topic
   Serial.print("Topic: ");
   Serial.println(topic);
- 
-  // Print the message
-  Serial.print("Message: ");
-  for(int i = 0; i < length; i ++)
-  {
-    Serial.print(char(payload[i]));
+
+  if (strcmp(topic, topic_current_subscribe) == 0) {
+    current_output = (int)(str_message.toFloat() * (4095.0 / current_max));
+    Serial.println(current_output);
+    dac.setValue(current_output);
   }
- 
-  // Print a newline
-  Serial.println("");
-}
+  else if (strcmp(topic,topic_voltage_subscribe)==0) {
+    voltage_output = (int)(str_message.toFloat() * (4095.0 / voltage_max));
+    //dac.setValue(current_output)
+    Serial.println("Voltage Output not configured");
+  }
+  else {
+    // Print the message
+    Serial.print("Message: ");
+    Serial.println(str_message);
+
+    }
+  }
+
